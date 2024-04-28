cf42515a2df5d2440862869f5c8375ebd305592e
Caleb <caleb_cyr@uri.edu>
2024-04-27 22:48:44 -0400

    changed opt-level to z

---
+++ rum/target/release/deps/rum-d77f96b868854e36.d
@@ -0,0 +1,11 @@
+/mnt/h/School/CSC-411/Assignments/Profiling/rum/target/release/deps/librum-d77f96b868854e36.rmeta: src/lib.rs src/rumdis.rs src/rumload.rs src/universal_machine.rs src/register.rs
+
+/mnt/h/School/CSC-411/Assignments/Profiling/rum/target/release/deps/librum-d77f96b868854e36.rlib: src/lib.rs src/rumdis.rs src/rumload.rs src/universal_machine.rs src/register.rs
+
+/mnt/h/School/CSC-411/Assignments/Profiling/rum/target/release/deps/rum-d77f96b868854e36.d: src/lib.rs src/rumdis.rs src/rumload.rs src/universal_machine.rs src/register.rs
+
+src/lib.rs:
+src/rumdis.rs:
+src/rumload.rs:
+src/universal_machine.rs:
+src/register.rs:
