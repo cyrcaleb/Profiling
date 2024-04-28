58235dcb4db0738d579419422a9a93504df769a2
Caleb <caleb_cyr@uri.edu>
2024-04-28 00:16:19 -0400

    changed set_val_memory_space to use if let statment

--- rum/src/universal_machine.rs
+++ rum/src/universal_machine.rs
@@ -1,17 +1,16 @@
 use crate::rumdis;
 
 pub struct UniversalMachine {
-    pub registers: Vec<u32>,
+    pub registers: [u32; 8],
     pub program_counter: u32,
     pub memory_space: Vec<Vec<u32>>,
     pub free_memory: Vec<u32>,
-} 
+}
 
 impl UniversalMachine {
     pub unsafe fn new(instructions: Vec<u32>) -> Self {
-        let mut registers = Vec::with_capacity(8);
-        registers.resize(8, 0);  
-        let memory_space = vec![instructions]; 
+        let registers = [0; 8];
+        let memory_space = vec![instructions];
         let program_counter = 0;
         let free_memory = Vec::new();
 
@@ -23,28 +22,15 @@ impl UniversalMachine {
         }
     }
 
-    pub unsafe fn get_segment_from_memory_space(&self, address: u32) -> &[u32] {
-        self.memory_space.get_unchecked(address as usize)
-    }
-
-    pub unsafe fn set_segment_from_memory_space(&mut self, address: u32, value: Vec<u32>) {
-        if (address as usize) < self.memory_space.len() {
-            self.memory_space[address as usize] = value;
-        }
-    }
-
     pub unsafe fn get_val_from_memory_space(&self, address: u32, offset: u32) -> u32 {
         let segment = self.memory_space.get_unchecked(address as usize);
         *segment.get_unchecked(offset as usize)
     }
 
     pub unsafe fn set_val_from_memory_space(&mut self, address: u32, offset: u32, value: u32) {
-        let segment = self.memory_space.get_unchecked_mut(address as usize);
-        *segment.get_unchecked_mut(offset as usize) = value;
-    }
-
-    pub unsafe fn get_from_free_memory(&self) -> u32 {
-        self.free_memory[self.free_memory.len() - 1]
+        if let Some(segment) = self.memory_space.get_mut(address as usize) {
+            *segment.get_unchecked_mut(offset as usize) = value;
+        }
     }
 
     pub unsafe fn run(&mut self) {
