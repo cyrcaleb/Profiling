ac1624cad9ed0ea456200113b989e086c2468377
Caleb <caleb_cyr@uri.edu>
2024-04-27 23:32:07 -0400

    removed bounds checking where it was not needed

--- rum/src/register.rs
+++ rum/src/register.rs
@@ -12,14 +12,14 @@ impl UniversalMachine {
     pub unsafe fn load(&mut self, reg1: u32, reg2: u32, reg3: u32) {
         let reg2_value = *self.registers.get_unchecked(reg2 as usize);
         let reg3_value = *self.registers.get_unchecked(reg3 as usize);
-        *self.registers.get_unchecked_mut(reg1 as usize) = self.get_val_from_memory_space(reg2_value, reg3_value);
+        *self.registers.get_unchecked_mut(reg1 as usize) = *self.memory_space.get_unchecked(reg2_value as usize).get_unchecked(reg3_value as usize);
     }
     
     pub unsafe fn store(&mut self, reg1: u32, reg2: u32, reg3: u32) {
         let reg1_value = *self.registers.get_unchecked(reg1 as usize);
         let reg2_value = *self.registers.get_unchecked(reg2 as usize);
         let reg3_value = *self.registers.get_unchecked(reg3 as usize);
-        self.set_val_from_memory_space(reg1_value, reg2_value, reg3_value);
+        *self.memory_space.get_unchecked_mut(reg1_value as usize).get_unchecked_mut(reg2_value as usize) = reg3_value;
     }
 
     pub unsafe fn add(&mut self, reg1: u32, reg2: u32, reg3: u32) {
@@ -35,10 +35,11 @@ impl UniversalMachine {
     }
     
     pub unsafe fn div(&mut self, reg1: u32, reg2: u32, reg3: u32) {
-        let reg2_value = *self.registers.get_unchecked(reg2 as usize);
         let reg3_value = *self.registers.get_unchecked(reg3 as usize);
-        // if reg3_value == 0 { panic!("Cannot Divide By 0") }
-        *self.registers.get_unchecked_mut(reg1 as usize) = reg2_value.wrapping_div(reg3_value);
+        if reg3_value != 0 {
+            let reg2_value = *self.registers.get_unchecked(reg2 as usize);
+            *self.registers.get_unchecked_mut(reg1 as usize) = reg2_value.wrapping_div(reg3_value);
+        }
     }
     
     pub unsafe fn nand(&mut self, reg1: u32, reg2: u32, reg3: u32) {
@@ -52,39 +53,37 @@ impl UniversalMachine {
     }
     
     pub unsafe fn mapseg(&mut self, reg1: u32, reg2: u32) {
-        if !self.free_memory.is_empty() {
+        if let Some(&last_free_mem) = self.free_memory.last() {
             let num_words = *self.registers.get_unchecked(reg2 as usize);
             let segment = vec![0; num_words as usize];
-            self.set_segment_from_memory_space(self.free_memory[(self.free_memory.len() - 1) as usize], segment);
-            *self.registers.get_unchecked_mut(reg1 as usize) = self.free_memory[(self.free_memory.len() - 1) as usize];
-            self.free_memory.pop().unwrap();
+            *self.memory_space.get_unchecked_mut(last_free_mem as usize) = segment;
+            *self.registers.get_unchecked_mut(reg1 as usize) = last_free_mem;
+            self.free_memory.pop();
             return;
         }
         let num_words = *self.registers.get_unchecked(reg2 as usize);
         let segment = vec![0; num_words as usize];
         self.memory_space.push(segment);
-        *self.registers.get_unchecked_mut(reg1 as usize) = (self.memory_space.len().wrapping_sub(1)) as u32;
+        *self.registers.get_unchecked_mut(reg1 as usize) = (self.memory_space.len() - 1) as u32;
     }
     
     pub unsafe fn unmapseg(&mut self, reg1: u32) {
         let reg1_value = *self.registers.get_unchecked(reg1 as usize);
-        self.set_segment_from_memory_space(reg1_value, vec![0]);
+        self.memory_space.get_unchecked_mut(reg1_value as usize).clear();
         self.free_memory.push(reg1_value);
     }
     
     pub unsafe fn output(&mut self, reg1: u32) {
         let reg_value = *self.registers.get_unchecked(reg1 as usize) as u8;
-        let output_buffer = &[reg_value];
-        stdout().write_all(output_buffer).unwrap();
+        stdout().write_all(&[reg_value]).unwrap();
         stdout().flush().unwrap();
     }
     
     pub unsafe fn input(&mut self, reg1: u32) {
-        let mut input_buffer = [0_u8; 1]; // Buffer to store the input byte
+        let mut input_buffer = [0_u8; 1];
         if let Ok(n) = stdin().read(&mut input_buffer) {
-            if n > 0 { // If a byte was successfully read
-                let input_value = input_buffer[0] as u32;
-                *self.registers.get_unchecked_mut(reg1 as usize) = input_value;
+            if n > 0 {
+                *self.registers.get_unchecked_mut(reg1 as usize) = input_buffer[0] as u32;
             }
         } 
     }
@@ -92,11 +91,11 @@ impl UniversalMachine {
     pub unsafe fn loadprog(&mut self, reg1: u32, reg2: u32) {
         let reg1_value = *self.registers.get_unchecked(reg1 as usize);
         if reg1_value == 0 {
-            self.set_program_counter(*self.registers.get_unchecked(reg2 as usize));
+            self.program_counter = *self.registers.get_unchecked(reg2 as usize);
         } else {
-            let segment = self.get_segment_from_memory_space(reg1_value).clone();
-            self.set_segment_from_memory_space(0, segment);
-            self.set_program_counter(*self.registers.get_unchecked(reg2 as usize));
+            let segment = self.memory_space.get_unchecked(reg1_value as usize).clone();
+            *self.memory_space.get_unchecked_mut(0) = segment;
+            self.program_counter = *self.registers.get_unchecked(reg2 as usize);
         }
     }
     
